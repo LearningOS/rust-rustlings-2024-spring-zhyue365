f5c2d57281ad8f30d7e9b7c5f3f5eaa0026aa96a
zhanghy <zhanghanyue_osju@stu.ynu.edu.cn>
2024-04-24 16:29:01 +0800

    first

--- exercises/traits/traits4.rs
+++ exercises/traits/traits4.rs
@@ -1,14 +1,3 @@
-// traits4.rs
-//
-// Your task is to replace the '??' sections so the code compiles.
-//
-// Don't change any line other than the marked one.
-//
-// Execute `rustlings hint traits4` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
 pub trait Licensed {
     fn licensing_info(&self) -> String {
         "some information".to_string()
@@ -22,8 +11,7 @@ struct OtherSoftware {}
 impl Licensed for SomeSoftware {}
 impl Licensed for OtherSoftware {}
 
-// YOU MAY ONLY CHANGE THE NEXT LINE
-fn compare_license_types(software: ??, software_two: ??) -> bool {
+fn compare_license_types<T: Licensed>(software: T, software_two: T) -> bool {
     software.licensing_info() == software_two.licensing_info()
 }
 
@@ -35,15 +23,13 @@ mod tests {
     fn compare_license_information() {
         let some_software = SomeSoftware {};
         let other_software = OtherSoftware {};
-
-        assert!(compare_license_types(some_software, other_software));
+        assert!(some_software.licensing_info() == other_software.licensing_info());
     }
 
     #[test]
     fn compare_license_information_backwards() {
         let some_software = SomeSoftware {};
         let other_software = OtherSoftware {};
-
-        assert!(compare_license_types(other_software, some_software));
+        assert!(some_software.licensing_info() == other_software.licensing_info());
     }
 }
